f3f10761433ea927636cc71272a7435b99ff8c10
nico <nivanov4@jhu.edu>
2020-05-28 12:29:24 -0400

    added user login on commenting and changed comments so that the commenter is the users email

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,23 +1,23 @@
 class ArticlesController < ApplicationController
 
-  before_action :find_all_articles, only: [:index]
   before_action :find_article_by_id, only: [:show, :edit, :update, :destroy]
-  before_action :make_new_article, only: [:new]
-  before_action :find_new_article, only: [:create]
-   
+
   def index
+    @articles = Article.all
   end
 
   def show
   end
   
   def new
+    @article = Article.new
   end
 
   def edit
   end
   
   def create
+    @article = Article.new(article_params)
     if @article.save
       redirect_to @article
     else
@@ -43,19 +43,8 @@ class ArticlesController < ApplicationController
     params.require(:article).permit(:title, :text)
   end
 
-  def find_all_articles
-    @articles = Article.all
-  end
-
   def find_article_by_id
     @article = Article.find(params[:id])
   end
-
-  def make_new_article
-    @article = Article.new
-  end
   
-  def find_new_article
-    @article = Article.new(article_params)
-  end
 end
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,9 +1,11 @@
 class CommentsController < ApplicationController
 
   before_action :find_article_by_id
-
+  before_action :authenticate_user!
+  
   def create
     @comment = @article.comments.create(comment_params)
+    @comment.update(:commenter => current_user.email)
     redirect_to article_path(@article)
   end
 
@@ -15,7 +17,7 @@ class CommentsController < ApplicationController
   
   private
   def comment_params
-    params.require(:comment).permit(:commenter, :body)
+    params.require(:comment).permit(:body)
   end
 
   def find_article_by_id
